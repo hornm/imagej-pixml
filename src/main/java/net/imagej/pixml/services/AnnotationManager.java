2d38564c5b689ff11d39c139cf6b22d41975ef88
hornm <horn_martin@gmx.de>
2016-06-08 15:57:22 +0200

    ... playing ...

---
+++ src/main/java/net/imagej/pixml/services/AnnotationManager.java
@@ -0,0 +1,61 @@
+package net.imagej.pixml.services;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.scijava.command.Command;
+import org.scijava.display.Display;
+import org.scijava.display.DisplayService;
+import org.scijava.event.EventHandler;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import net.imagej.display.ImageDisplay;
+import net.imagej.display.OverlayService;
+import net.imagej.event.OverlayCreatedEvent;
+import net.imagej.overlay.Overlay;
+
+@Plugin(type = Command.class, label = "Annotation Manager")
+public class AnnotationManager implements Command {
+
+	@Parameter
+	private OverlayService overlayService;
+
+	@Parameter
+	private DisplayService displayService;
+
+	@Override
+	public void run() {
+		JFrame f = new JFrame("Annotation Manager");
+		Display<?> display = displayService.getActiveDisplay();
+		List<Overlay> overlays;
+		if (display instanceof ImageDisplay) {
+			overlays = overlayService.getOverlays((ImageDisplay) display);
+		} else {
+			overlays = Collections.emptyList();
+		}
+		f.setLayout(new GridLayout(overlays.size() + 1, 1));
+		f.add(new JLabel("Overlay -> Class Label"));
+		for (int i = 0; i < overlays.size(); i++) {
+			JPanel p = new JPanel(new FlowLayout());
+			p.add(new JLabel("overlay " + i));
+			p.add(new JTextField("class1"));
+			f.add(p);
+		}
+		f.pack();
+		f.setVisible(true);
+	}
+
+	@EventHandler
+	private void onEvent(OverlayCreatedEvent e) {
+		//
+	}
+
+}
--- src/main/java/net/imagej/pixml/services/AnnotationService.java
+++ src/main/java/net/imagej/pixml/services/AnnotationService.java
@@ -1,8 +1,28 @@
 package net.imagej.pixml.services;
 
-import net.imagej.ImageJService;
+import org.scijava.service.SciJavaService;
+import org.scijava.tool.Tool;
 
-public interface AnnotationService extends ImageJService {
+import net.imagej.display.ImageDisplay;
+import net.imagej.ui.swing.commands.OverlayManager;
+import net.imglib2.roi.labeling.ImgLabeling;
+import net.imglib2.roi.labeling.LabelRegions;
+
+/**
+ * Possible entry point to get image annotations.
+ * 
+ * Thoughts:
+ * - should give either {@link ImgLabeling} or {@link LabelRegions} (and some how access to their source images -> RichImg?) -> in that way a label basically associated with a pixel (or rather a pixel position)
+ * - maybe should be able to retrieve the annotations from the {@link OverlayManager}? Question here: where come the labels/classes from?
+ * - need of another source of annotations (overlays are not sufficient because of missing label information): kind of a annotation manager using {@link Tool}s to draw, add a label to them and generate, e.g., an {@link ImgLabeling} out of it
+ * - drawing on a big data viewer display should be possible
+ * - how can we make use of that stuff in KNIME? i.e. Image Annotator-node ...
+ * - could maybe provide a method to retrieve an {@link ImgLabeling} from an {@link ImageDisplay}
+ * 
+ * @author Martin Horn
+ *
+ */
+public interface AnnotationService extends SciJavaService {
 
 	//pixel annotation service -> e.g. gives ImgLabelings or LabelRegions to represent image annotations
 	
