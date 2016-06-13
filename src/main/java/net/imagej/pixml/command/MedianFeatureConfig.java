cfb893d9aa76cc755af96f74b9e3006750e3a805
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2016-06-13 16:57:36 +0200

    Add median and minimum features

---
+++ src/main/java/net/imagej/pixml/command/MedianFeatureConfig.java
@@ -0,0 +1,22 @@
+package net.imagej.pixml.command;
+
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class)
+public class MedianFeatureConfig implements Command{
+
+	@Parameter
+	private int radius = 1;
+	
+	@Override
+	public void run() {
+		//
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
+	
+}
